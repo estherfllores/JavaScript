60c114e9e0892ecd07d2a5e2b5630f0653b2290a
estherflores <estifloresolivares@gmail.com>
2023-12-12 00:14:25 -0300

    Entrega

---
+++ main.js
@@ -0,0 +1,16 @@
+const suma = (a,b) => a + b;
+const resta = (a,b) => a - b;
+let precioheladera = 150000;
+let descuento = 15000;
+let nuevoprecio = resta(precioheladera, descuento)
+
+alert(nuevoprecio);
+
+
+let respuesta = prompt ("Compra el producto?").toLowerCase();
+if (respuesta === "si") { alert("Gracias por su compra");
+
+} else { alert("Gracias por su visita")
+  
+};
+
